2cf4d19a9bcb96de4642864f8101e408bd1e8b3b
SamanthaSays <thedrdg2@gmail.com>
2024-04-30 09:58:46 +0100

    Changelog support

---
+++ assets/js/changelog.js
@@ -0,0 +1,7 @@
+function changelog() {
+    if (document.getElementById("changelog").style.display === "none") {
+        document.getElementById("changelog").style.display = "block";
+    } else {
+        document.getElementById("changelog").style.display = "none";
+    }
+}
