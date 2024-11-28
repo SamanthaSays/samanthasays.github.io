987accf8c54028651f1684a3940e04daf9141675
SamanthaSays <thedrdg2@gmail.com>
2024-11-28 09:17:10 +0000

    Updated merch, added tip jar

--- assets/js/randomMerch.js
+++ assets/js/randomMerch.js
@@ -1,2 +1,2 @@
 var merchNum = Math.floor(Math.random() * 4 ) + 1;
-document.getElementById("randomMerch").src= '/assets/images/DBH-'+merchNum+' 300x250.webp';
+document.getElementById("randomMerch").src= '/assets/images/Merch-'+merchNum+' 300x250.webp';
